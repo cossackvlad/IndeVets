3ec1f8e74a5570e7b76512e56ed9b6043de82ca1
Vlad <v.kotenko95@gmail.com>
2021-06-23 18:53:16 +0300

    first commit

---
+++ js/style.js
@@ -0,0 +1,17 @@
+// Menu
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu-mobile'),
+      close = document.querySelector('.close'),
+      menuLink = document.querySelector('.menu__top-box');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuLink.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
